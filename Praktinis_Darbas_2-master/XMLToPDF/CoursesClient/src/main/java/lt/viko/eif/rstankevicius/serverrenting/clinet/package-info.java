55d84db1b5fb741e8a59e19c481d091442399c3f
arnas159632 <89695189+arnas159632@users.noreply.github.com>
2023-08-30 12:55:03 +0300

    Add files via upload

---
+++ Praktinis_Darbas_2-master/XMLToPDF/CoursesClient/src/main/java/lt/viko/eif/rstankevicius/serverrenting/clinet/package-info.java
@@ -0,0 +1,2 @@
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.ServerRenting.lt/viko/eif/adrevinskas/serverrenting/api/userDataCheck", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package lt.viko.eif.adrevinskas.serverrenting.clinet;
