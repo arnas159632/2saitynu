aed81e36f5471e02494b098b626937c22d600ccf
arnas159632 <89695189+arnas159632@users.noreply.github.com>
2023-08-30 12:53:25 +0300

    Delete Praktinis_Darbas_2-master directory

--- Praktinis_Darbas_2-master/CoursesClient/src/main/java/lt/viko/eif/adrevinskas/courses/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.Courses.lt/viko/eif/adrevinskas/courses/api/userDataCheck", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package lt.viko.eif.adrevinskas.courses.client;
